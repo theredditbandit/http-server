53acb903a67cdb9d86b034bbd9947f9088727453
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-10-09 12:45:51 +0530

    reply with user-agent

--- app/libs/server.py
+++ app/libs/server.py
@@ -30,15 +30,20 @@ class Server:
             data (bytes): Request data
         """
         request = request.split(DELIM.encode())
-        _, path, _ = request[
+        _, resource, _ = request[
             HTTP_HEADER_MAP["req_line"]
         ].split()  # [b'GET', b'/index.html', b'HTTP/1.1']
 
-        path = path.decode()
-        if path == "/":
+        resource :str = resource.decode()
+        if resource == "/":
             response = f"{RESPONSE_MAP['status'][200]}{EOF}"
-        elif str(path).startswith("/echo"):
-            content = str(path).split("/", maxsplit=2)[-1]
+        elif resource.startswith("/echo"):
+            content = resource.split("/", maxsplit=2)[-1]
+            response = f"{RESPONSE_MAP['status'][200]}{DELIM}Content-Type: text/plain{DELIM}Content-Length: {len(content)}{EOF}{content}"
+        elif resource.startswith('/user-agent'):
+            user_agent = request[HTTP_HEADER_MAP["User-Agent"]]
+            _ , content = user_agent.decode().split(':')
+            content = content.strip()
             response = f"{RESPONSE_MAP['status'][200]}{DELIM}Content-Type: text/plain{DELIM}Content-Length: {len(content)}{EOF}{content}"
         else:
             response = f"{RESPONSE_MAP['status'][404]}{EOF}"
--- app/libs/static/fields.py
+++ app/libs/static/fields.py
@@ -1,5 +1,9 @@
 RESPONSE_MAP = {"status": {200: "HTTP/1.1 200 OK", 404: "HTTP/1.1 404 Not Found"}}
 
-HTTP_HEADER_MAP = {"req_line": 0}
+HTTP_HEADER_MAP = {"req_line": 0,
+                   "Host":1,
+                   "User-Agent":2
+                   }
+
 EOF = "\r\n\r\n"
 DELIM = "\r\n"
