f940ee521c399290fab9e1c56064b72d5a91bcab
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-10-15 22:35:57 +0530

    minor changes 2

--- app/libs/server/basicServer.py
+++ app/libs/server/basicServer.py
@@ -8,8 +8,7 @@ class Server:
     def __init__(self, host="localhost", port=4221):
         self.host = host
         self.port = port
-        print(f"Host: {host} and Port: {port}")
-
+        
     async def response_handler(self, request: bytes, writer: StreamWriter) -> bytes:
         """Builds and sends a response to http requests per the request.
 
@@ -26,12 +25,14 @@ class Server:
             response = f"{RESPONSE_MAP['status'][200]}{EOF}"
         elif resource.startswith("/echo"):
             content = resource.split("/", maxsplit=2)[-1]
-            response = f"{RESPONSE_MAP['status'][200]}{DELIM}Content-Type: text/plain{DELIM}Content-Length: {len(content)}{EOF}{content}"
+            response = f"{RESPONSE_MAP['status'][200]}{DELIM}Content-Type: text/plain{DELIM}\
+                Content-Length: \{len(content)}{EOF}{content}"
         elif resource.startswith("/user-agent"):
             user_agent = request[HTTP_HEADER_MAP["User-Agent"]]
             _, content = user_agent.decode().split(":")
             content = content.strip()
-            response = f"{RESPONSE_MAP['status'][200]}{DELIM}Content-Type: text/plain{DELIM}Content-Length: {len(content)}{EOF}{content}"
+            response = f"{RESPONSE_MAP['status'][200]}{DELIM}Content-Type: text/plain{DELIM}\
+                Content-Length: {len(content)}{EOF}{content}"
         else:
             response = f"{RESPONSE_MAP['status'][404]}{EOF}"
 
@@ -40,7 +41,6 @@ class Server:
         writer.write(response)
         await writer.drain()
         writer.close()
-
 
     async def request_handler(self, reader: StreamReader, writer: StreamWriter):
         data: bytes = await reader.readuntil(EOF.encode())
